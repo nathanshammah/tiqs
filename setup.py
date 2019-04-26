d438b5c35094270b58db105bdfde9b1d79e66cfe
Nathan Shammah <nathan.shammah@gmail.com>
2019-04-26 17:29:43 +0900

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ from setuptools import setup
 
 setup(name='tiqs',
       version='0.1',
-      description='Test',
+      description='Traslational Invariant Quantum Solver',
+      url='https://github.com/fminga/tiqs'
       author='Fabrizio Minganti, Nathan Shammah',
+      license='BSD 3 New'
       packages = ['tiqs']
      )
